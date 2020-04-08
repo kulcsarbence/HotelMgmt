a361d035a602000ce978f71e5e6027fcead236f9
kulcsarbence <bance98@gmail.com>
2020-04-08 17:17:47 +0200

    minor changes to customer.hpp

--- HotelManagement/HotelManagement/Customer.hpp
+++ HotelManagement/HotelManagement/Customer.hpp
@@ -1,4 +1,6 @@
 #pragma once
+
+#define _CRT_SECURE_NO_WARNINGS
 #include <string>
 #include <ctime>
 
@@ -18,4 +20,55 @@ private:
 	Date to;
 	double payment;
 public:
+	Customer(const std::string& fName, const std::string& lName, const std::string& addr, const std::string& phone, const double& payment, const Date& from, const 
+	Date& to) : firstName(fName), lastName(lName), address(addr), phoneNumber(phone), from(from), to(to), payment(payment){}
+	Date getTimeRemainingTillCheckout() {
+		time_t now = time(0);
+		tm *ltm = localtime(&now);
+		Date toReturn{ 1900 + ltm->tm_year , 1 + ltm->tm_mon ,  ltm->tm_mday };
+		return toReturn;
+	}
+	std::string getFirstName() {
+		return firstName;
+	}
+	std::string getLastName() {
+		return lastName;
+	}
+	std::string getAddress() {
+		return address;
+	}
+	std::string getPhoneNumber() {
+		return phoneNumber;
+	}
+	Date getDateFrom() {
+		return from;
+	}
+	Date getDateTo() {
+		return to;
+	}
+	double getPayment() {
+		return payment;
+	}
+	void setFirstName(const std::string& s) {
+		firstName = s;
+	}
+	void setLastName(const std::string& s) {
+		lastName = s;
+	}
+	void setAddress(const std::string& s) {
+		address = s;
+	}
+	void setPhoneNumber(const std::string& s) {
+		phoneNumber = s;
+	}
+	void setDateFrom(const Date& from) {
+		this->from = from;
+	}
+	void setDateTo(const Date& to) {
+		this->to = to;
+	}
+	void setPayment(const double& s) {
+		payment = s;
+	}
+	~Customer(){}
 };
--- HotelManagement/HotelManagement/HotelManagement.cpp
+++ HotelManagement/HotelManagement/HotelManagement.cpp
@@ -1,13 +1,12 @@
 //uniform inicializacio
 //auto
 //smart pointers
-
 #include "Customer.hpp"
 #include "Room.hpp"
 #include "Hotel.hpp"
 #include <iostream>
 
-
+using namespace std;
 int main()
 {
     return 0;
